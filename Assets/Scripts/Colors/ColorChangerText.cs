fad105101a085f0f74d4cf5c1cc537998ba26b83
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-08-14 14:30:31 -0400

    scrolling text

---
+++ Assets/Scripts/Colors/ColorChangerText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChangerText : MonoBehaviour {
+
+    public float changeDelay;
+
+    private Color color;
+    private TextMesh text;
+    private float changeTimer;
+
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+        changeTimer = changeDelay;
+    }
+	
+	void Update () {
+        if(changeTimer <= 0.0f)
+        {
+            color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            text.color = color;
+            changeTimer = changeDelay;
+        }
+        changeTimer -= Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Text/ScrollText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollText : MonoBehaviour {
+
+    public float speed;
+    public float initalDelay;
+
+	void Update () {
+        if(initalDelay <= 0.0f)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (transform.position.x < -27.0f)
+            {
+                transform.Translate(Vector3.right * 36.5f);
+            }
+        }
+        else
+        {
+            initalDelay -= Time.deltaTime;
+        }
+	}
+}
